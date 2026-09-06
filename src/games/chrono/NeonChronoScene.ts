81e07b85c8f5672348f16eae9836266af5ed3a8c
BiosSystem <63607038+BiosSystem@users.noreply.github.com>
2026-09-07 00:34:50 +0300

    feat(visuals): enrich chrono chamber materials

--- src/games/chrono/NeonChronoScene.ts
+++ src/games/chrono/NeonChronoScene.ts
@@ -67,17 +67,19 @@ export default class NeonChronoScene extends Phaser.Scene {
 
   private draw(delta: number) {
     const fog = integrateAtmosphere(80, .62, .7, this.raymarch.adapt(delta));
-    this.gfx.clear().fillStyle(0x030510).fillRect(0, 0, 640, 480);
-    for (let i = 0; i < 18; i++) { const alpha = .02 + fog.luminance * .8; this.gfx.fillStyle(i % 2 ? 0x2244ff : 0x00ffcc, alpha).fillCircle((i * 83 + this.frame) % 680 - 20, 70 + i * 21, 30 + i % 4 * 12); }
-    this.gfx.fillStyle(0x182044).fillRect(0, 425, 640, 20).fillRect(0, 45, 640, 18);
-    for (const platform of this.chamber.platforms) this.gfx.fillStyle(0x315080).fillRect(platform.x, platform.y, platform.width, 8);
-    this.gfx.fillStyle(0x2566ff, .18).fillCircle(this.chamber.slowField.x, 300, this.chamber.slowField.radius);
-    this.gfx.fillStyle(0xff2ec4, .12).fillRect(this.chamber.gravityZone.x, 60, this.chamber.gravityZone.width, 365);
-    for (const laser of this.chamber.lasers) this.gfx.fillStyle(0xff2255, .55).fillRect(laser.x, laser.y, 3, laser.height);
-    this.gfx.fillStyle(this.state.gateOpen ? 0x00ff6e : 0xff2255).fillRect(this.chamber.gateX, 280, 10, 145);
-    this.gfx.fillStyle(0xffcc00).fillRect(this.chamber.switchX - 16, 418, 32, 7);
-    for (const clone of this.clones.values()) this.gfx.fillStyle(0x00aaff, .5).fillRect(clone.x - 9, clone.y - 30, 18, 30);
-    this.gfx.fillStyle(this.rewinding ? 0xff2ec4 : 0x00ffcc).fillRect(this.state.x - 9, this.state.y - 30, 18, 30);
+    this.gfx.clear().fillGradientStyle(0x030510, 0x07122b, 0x11113a, 0x030510).fillRect(0, 0, 640, 480);
+    for (let i = 0; i < 18; i++) { const alpha = .025 + fog.luminance * .35; this.gfx.fillStyle(i % 2 ? 0x2244ff : 0x00ffcc, alpha).fillCircle((i * 83 + this.frame * .35) % 680 - 20, 76 + i * 19, 34 + i % 4 * 13); }
+    for (let x = 28; x < 640; x += 64) this.gfx.lineStyle(1, 0x4d7dba, .12).lineBetween(x, 63, x - 42, 425);
+    for (let y = 105; y < 425; y += 48) this.gfx.lineStyle(1, 0x31527f, .14).lineBetween(0, y, 640, y);
+    this.gfx.fillStyle(0x0b1730, .92).fillRect(0, 425, 640, 20).fillRect(0, 45, 640, 18).lineStyle(1, 0x4ca6dd, .48).lineBetween(0, 63, 640, 63).lineBetween(0, 425, 640, 425);
+    for (const platform of this.chamber.platforms) this.gfx.fillStyle(0x172b4e).fillRect(platform.x, platform.y, platform.width, 10).fillStyle(0x62d8ff, .85).fillRect(platform.x, platform.y, platform.width, 2).lineStyle(1, 0x38679a, .8).strokeRect(platform.x, platform.y, platform.width, 10);
+    this.gfx.fillStyle(0x2566ff, .1).fillCircle(this.chamber.slowField.x, 300, this.chamber.slowField.radius).lineStyle(2, 0x4e8cff, .45).strokeCircle(this.chamber.slowField.x, 300, this.chamber.slowField.radius * .72).lineStyle(1, 0x94bcff, .35).strokeCircle(this.chamber.slowField.x, 300, this.chamber.slowField.radius * .42);
+    this.gfx.fillStyle(0xff2ec4, .09).fillRect(this.chamber.gravityZone.x, 63, this.chamber.gravityZone.width, 362).lineStyle(1, 0xff77de, .45).strokeRect(this.chamber.gravityZone.x, 63, this.chamber.gravityZone.width, 362);
+    for (const laser of this.chamber.lasers) this.gfx.fillStyle(0xff2255, .16).fillRect(laser.x - 3, laser.y, 9, laser.height).fillStyle(0xffb2c9, .9).fillRect(laser.x, laser.y, 3, laser.height);
+    const gateColor = this.state.gateOpen ? 0x00ff6e : 0xff2255; this.gfx.fillStyle(gateColor, .2).fillRect(this.chamber.gateX - 6, 275, 22, 150).lineStyle(2, gateColor, .92).strokeRect(this.chamber.gateX - 4, 280, 18, 145);
+    this.gfx.fillStyle(0x684c0a).fillRect(this.chamber.switchX - 18, 415, 36, 10).fillStyle(0xffd75a).fillRect(this.chamber.switchX - 14, 418, 28, 4);
+    for (const clone of this.clones.values()) { this.gfx.fillStyle(0x00aaff, .16).fillCircle(clone.x, clone.y - 20, 20).fillStyle(0x3fd8ff, .58).fillRoundedRect(clone.x - 8, clone.y - 27, 16, 24, 4).fillStyle(0xc1f8ff, .7).fillCircle(clone.x, clone.y - 31, 6); }
+    const playerColor = this.rewinding ? 0xff2ec4 : 0x00ffcc; this.gfx.fillStyle(playerColor, .16).fillCircle(this.state.x, this.state.y - 19, 23).fillStyle(playerColor, .92).fillRoundedRect(this.state.x - 8, this.state.y - 27, 16, 24, 4).fillStyle(0xe7ffff).fillCircle(this.state.x, this.state.y - 31, 6).lineStyle(2, playerColor, .85).lineBetween(this.state.x - 10, this.state.y, this.state.x + 10, this.state.y);
     const stats = this.ring.stats(); this.hud.setText(`FRAME ${this.frame}  HISTORY ${(stats.frames / 60).toFixed(1)}S  DELTA ${stats.ratio.toFixed(2)}X  CLONES ${this.clones.size}  FOG ${this.raymarch.count()} STEPS${this.rewinding ? '  REWIND' : ''}`);
   }
 
